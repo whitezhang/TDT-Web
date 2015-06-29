e78428c762d53e0d2b707f1a60294fdfac6125c8
Huaizhi Zhang <whitezhangv5@gmail.com>
2015-06-29 15:53:24 +0100

    dao; ;

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
+	"os"
+	"strings"
 )
 
 var top_words_path = "../plsa/model/top_words"
@@ -14,12 +18,45 @@ type TopicModels struct {
 	Topics []string
 }
 
+/*
+ * Usage: load topic models from file
+ * @param: filename string = top_words_path + "txt"
+ */
 func loadTopicModels(filepath string) (*TopicModels, error) {
 	filename := filepath + ".txt"
 	fmt.Println(filename)
-	var topics [2]string
-	topics[0] = "Sport Nice"
-	topics[1] = "Nice Sport"
+
+	// Read files
+	fin, err := os.Open(filename)
+	defer fin.Close()
+	if err != nil {
+		panic(err)
+		return nil, err
+	}
+	reader := bufio.NewReader(fin)
+	var topics [20]string
+	index := -1
+	num_topics := 0
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil || io.EOF == err {
+			break
+		}
+
+		if strings.Contains(line, "----") {
+			continue
+		} else if strings.Contains(line, "Topic #") {
+			index++
+			num_topics = 0
+		} else if num_topics > 5 {
+			continue
+		} else {
+			info := strings.Split(line, " ")
+			topics[index] += info[0] + " "
+			num_topics++
+		}
+	}
+
 	return &TopicModels{Topics: topics[:]}, nil
 	// return &TopicModels{Topics: "t111est"}, nil
 }
@@ -39,9 +76,6 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// page, _ := loadTopicModels("topics")
-	// fmt.Println("%v\n", page.Topics)
-
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/index", viewHandler)
 	http.ListenAndServe(":8090", nil)
