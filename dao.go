21976b07910a49df08d54110e5fc802e6ac61121
Huaizhi Zhang <whitezhangv5@gmail.com>
2015-06-29 23:09:02 +0100

    newsdata

--- dao.go
+++ dao.go
@@ -8,7 +8,37 @@ import (
 )
 
 type Twitter struct {
-	Text string `"bson: "text"`
+	Text string `bson: "text"`
+}
+
+type NewsData struct {
+	Title     string `bson: "text"`
+	TimeStamp string `bson: "text"`
+	MainStory string `bson: "text"`
+}
+
+func getNewsData() (*[]NewsData, error) {
+	session, err := mgo.Dial("127.0.0.1")
+	if err != nil {
+		fmt.Println("Connect MongoDB failed")
+		return nil, err
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+	conn := session.DB("gms").C("newsdata")
+	// Find
+	var result []NewsData
+	err = conn.Find(nil).Select(bson.M{"title": 1, "timeStamp": 1, "mainStory": 1}).All(&result)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	for _, v := range result {
+		fmt.Println(v.Title)
+	}
+	return &result, nil
 }
 
 func getTwitters() (*[]Twitter, error) {
@@ -40,4 +70,5 @@ func getTwitters() (*[]Twitter, error) {
 func main() {
 	fmt.Println("...")
 	getTwitters()
+	getNewsData()
 }
