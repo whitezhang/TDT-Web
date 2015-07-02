6c73f04c35cfbff53bdb58fb9c7d9e45d952d02c
Huaizhi Zhang <whitezhangv5@gmail.com>
2015-07-02 18:02:33 +0100

    charts button

--- static/js/myCharts.js
+++ static/js/myCharts.js
@@ -1,24 +1,111 @@
+var options = {
+        //Boolean - If we show the scale above the chart data    
+        scaleOverlay : false,
+        
+        //Boolean - If we want to override with a hard coded scale
+        scaleOverride : false,
+        
+        //** Required if scaleOverride is true **
+        //Number - The number of steps in a hard coded scale
+        scaleSteps : null,
+        //Number - The value jump in the hard coded scale
+        scaleStepWidth : null,
+        //Number - The scale starting value
+        scaleStartValue : null,
+
+        //String - Colour of the scale line    
+        scaleLineColor : "rgba(0,0,0,1)",
+        
+        //Number - Pixel width of the scale line
+        scaleLineWidth : 1,
+
+        //Boolean - Whether to show labels on the scale    
+        scaleShowLabels : true,
+        
+        //Interpolated JS string - can access value
+        scaleLabel : "<%=value%>",
+        
+        //String - Scale label font declaration for the scale label
+        scaleFontFamily : "'Arial'",
+        
+        //Number - Scale label font size in pixels    
+        scaleFontSize : 12,
+        
+        //String - Scale label font weight style
+        scaleFontStyle : "normal",
+        
+        //String - Scale label font colour    
+        scaleFontColor : "#666",    
+        
+        ///Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines : true,
+        
+        //String - Colour of the grid lines
+        scaleGridLineColor : "rgba(0,0,0,.1)",
+        
+        //Number - Width of the grid lines
+        scaleGridLineWidth : 1,    
+        
+        //Boolean - Whether the line is curved between points
+        bezierCurve : true,
+        
+        //Boolean - Whether to show a dot for each point
+        pointDot : true,
+        
+        //Number - Radius of each point dot in pixels
+        pointDotRadius : 1,
+        
+        //Number - Pixel width of point dot stroke
+        pointDotStrokeWidth : 0.2,
+        
+        //Boolean - Whether to show a stroke for datasets
+        datasetStroke : true,
+        
+        //Number - Pixel width of dataset stroke
+        datasetStrokeWidth : 3,
+        
+        //Boolean - Whether to fill the dataset with a colour
+        datasetFill : true,
+        
+        //Boolean - Whether to animate the chart
+        animation : true,
+
+        //Number - Number of animation steps
+        animationSteps : 60,
+        
+        //String - Animation easing effect
+        animationEasing : "easeOutQuart",
+
+        //Function - Fires when the animation is complete
+        onAnimationComplete : null
+    }
+
+
+
 trendsInfo = $('#trendsInfo').data('info').slice(3);
 
 var numDays = 31
 var trendDataSets = [];
+var nameSet = [];
+
 eachTrend = trendsInfo.match(/(\w)*:\{\[(\d|\s)*]}/g);
+
 for (var i in eachTrend) {
     var dataMap = {};
-    dataMap["fillColor"] = "rgba(220,220,220,0.5)";
-    dataMap["strokeColor"] = "rgba(220,220,220,1)";
-    dataMap["pointColor"] = "rgba(220,220,220,1)";
-    dataMap["pointStrokeColor"] = "#fff";
+    dataMap["fillColor"] = "rgba(220,220,220,0.4)";
+    dataMap["strokeColor"] = "rgba(220,220,220,0.8)";
+    dataMap["pointColor"] = "rgba(220,220,220,0.8)";
     // name = "Scotland_Yard:"
     var name = eachTrend[i].match(/(\w)*:/g);
-    // dataStr = "0 7 4 3 1 4 5 2 7 3 2 1 8 5 9 4 4 1 0 4 3 4 10 8 10 4 8 3 9 3 11"
+    nameSet.push(name.slice(0, -1))
+        // dataStr = "0 7 4 3 1 4 5 2 7 3 2 1 8 5 9 4 4 1 0 4 3 4 10 8 10 4 8 3 9 3 11"
     var dataStr = eachTrend[i].match(/(\d)+/g);
-    
+
     var dataArray = new Array();
-    if(dataStr["30"] > 0) {
+    if (dataStr["30"] > 0) {
         numDays = 30;
     }
-    for(var j in dataStr) {
+    for (var j in dataStr) {
         dataArray[j] = parseInt(dataStr[j]);
     }
     dataMap['data'] = dataArray;
@@ -33,25 +120,28 @@ for (var i = 0; i <= numDays; i++) {
 var lineChartData = {
     labels: daysIndex,
     datasets: trendDataSets
-    // datasets: [{
-    //     fillColor: "rgba(220,220,220,0.5)",
-    //     strokeColor: "rgba(220,220,220,1)",
-    //     pointColor: "rgba(220,220,220,1)",
-    //     pointStrokeColor: "#fff",
-    //     data: [65, 59, 90, 81, 56, 55, 40]
-    // }, {
-    //     fillColor: "rgba(151,187,205,0)",
-    //     strokeColor: "rgba(151,187,205,1)",
-    //     pointColor: "rgba(151,187,205,1)",
-    //     pointStrokeColor: "#fff",
-    //     data: [28, 48, 40, 19, 96, 27, 100]
-    // }]
-
 };
 
-new Chart(document.getElementById("trendsLineChart").getContext("2d")).Line(lineChartData);
+var chart = new Chart(document.getElementById("trendsLineChart").getContext("2d")).Line(lineChartData, options);
 
+function showMyTrend(dataName) {
+    dataId = -1;
+    for (var i = 0; i < nameSet.length; i++) {
+        if (dataName == nameSet[i]) {
+            dataId = i;
+            break;
+        }
+    }
+    tmpDatasets = lineChartData["datasets"]
+    for (var i = 0; i < tmpDatasets.length; i++) {
+        tmpDatasets[i]["fillColor"] = "rgba(220,220,220,0.1)";
+        tmpDatasets[i]["strokeColor"] = "rgba(220,220,220,0.1)";
+        tmpDatasets[i]["pointColor"] = "rgba(220,220,220,0)";
+    }
 
-function showMyTrend() {
-    document.write("fsfsd");
-}
+    lineChartData["datasets"][dataId]["fillColor"] = "rgba(0,0,0,1)";
+    lineChartData["datasets"][dataId]["strokeColor"] = "rgba(0,0,0,1)";
+    lineChartData["datasets"][dataId]["pointColor"] = "rgba(0,0,0,1)";
+
+    chart = new Chart(document.getElementById("trendsLineChart").getContext("2d")).Line(lineChartData, options);
+};
